bd0b9c114006018630d5cdc4a7f27eefcc449fc6
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-07-21 00:28:16 +0300

    Fix ObfuscateDataId.Equals

--- src/SO.Abstraction/ObfuscateDataId.cs
+++ src/SO.Abstraction/ObfuscateDataId.cs
@@ -8,7 +8,7 @@ public readonly struct ObfuscateDataId(MDToken mdt, Type dataType) : IEquatable<
     private readonly MDToken m_Token = mdt;
     private readonly Type m_DataType = dataType;
 
-    public bool Equals(ObfuscateDataId other) => m_Token == other.m_Token;
+    public bool Equals(ObfuscateDataId other) => m_Token == other.m_Token && m_DataType == other.m_DataType;
 
     public override bool Equals(object obj) => obj is ObfuscateDataId rinfo && base.Equals(rinfo);
 
